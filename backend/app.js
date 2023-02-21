4bcb3053c1548053b500ebdce681d9e071f4bbe0
illimpassa <illimpassa@gmail.com>
2023-02-21 22:47:44 +0100

    Backend setup

--- backend/app.js
+++ backend/app.js
@@ -14,14 +14,13 @@ const app = express();
 // Configure Express App Instance
 app.use(express.json( { limit: '50mb' } ));
 app.use(express.urlencoded( { extended: true, limit: '10mb' } ));
-
-// Configure custom logger middleware
-// app.use(logger.dev, logger.combined);
-
 app.use(morgan('dev'));
 // app.use(cookieParser());
 app.use(cors());
 app.use(helmet());
+// Configure custom logger middleware
+// app.use(logger.dev, logger.combined);
+
 
 // This middleware adds the json header to every response
 app.use('*', (req, res, next) => {
@@ -29,10 +28,20 @@ app.use('*', (req, res, next) => {
     next();
 })
 
-
+//dabase connection
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('DB Connected'))
+.catch(err => {
+    console.log(`DB Connection Error: ${err.message}`);
+});
 
 // Handle errors
 // app.use(errorHandler());
+
+// Routes
+
 
 // Handle not valid route
 app.use('*', (req, res) => {
