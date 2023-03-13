3d57617a10e11f86d95a38c83f5d008f00adbda1
illimpassa <illimpassa@gmail.com>
2023-03-13 20:35:31 +0100

    Not important:all files pushed

---
+++ talent-tracker/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UploadExcel from './test/UploadExcel';
+
+function App() {
+  return (
+    <div className="App">
+      <UploadExcel />
+    </div>
+  );
+}
+
+export default App;
